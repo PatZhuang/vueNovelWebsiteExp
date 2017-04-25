153a00bb5ba00f176ce26b3ac60627f20f0db867
Pat <Pat@Pat.lan>
2017-04-25 20:00:40 +0800

    导航界面、banner 部分完成

---
+++ src/App.vue
@@ -0,0 +1,175 @@
+<style scoped>
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+.layout-logo {
+  width: 100%;
+  position: relative;
+  margin: auto;
+}
+
+.layout-header {
+  height: 60px;
+  background: #fff;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, .1);
+  display: flex;
+}
+
+.layout-copy {
+  text-align: center;
+  padding: 10px 0 20px;
+  color: #9ea7b4;
+}
+
+.layout-ceiling {
+  background: rgb(247, 247, 247);
+  padding: 10px 0;
+  overflow: hidden;
+}
+
+.layout-ceiling-right {
+  float: right;
+  margin-right: 15px;
+}
+
+.layout-ceiling-right a {
+  color: rgb(38, 38, 38);
+  margin-right: 5px;
+  margin-left: 5px;
+}
+
+#ceiling-register {
+  color: #aaa;
+}
+
+.layout-ceiling-right a:hover, #ceiling-register:hover{
+  /*登录、注册按钮 hover 状态颜色*/
+  color: rgb(234, 68, 92);
+}
+
+.layout-ceiling-right span {
+  color: #ccc;
+}
+</style>
+
+<template>
+  <div id="app">
+    <!--顶栏-->
+    <div class="layout-ceiling">
+      <div class="layout-ceiling-right">
+        <a href="#">登录</a> 
+        <span>|</span>
+        <a href="#" id="ceiling-register">注册</a>
+      </div>
+    </div>
+  
+    <!--Logo、搜索框一行-->
+    <Row type="flex" justify="center" align="middle" style="margin: 15px 0px; ">
+      <Col :xs="2" :sm="{span: 1, offset: 1}" class="Col-logo">
+        <img src="../src/assets/logo.png" alt="logo" id='logo' class="layout-logo">
+      </Col>
+      <Col :xs="0" :sm="2">
+        <h1>VueTest</h1>
+      </Col>
+      <Col :xs="{span: 14, offset: 2}" :sm="12">
+        <Input>
+          <Button slot="append" icon="ios-search"></Button>
+        </Input>
+      </Col>
+      <Col :xs="{span: 2, offset: 2}" :sm="4">
+        <Button type="ghost" icon="ios-bookmarks" @click="showNothing">
+          <span v-if="notXsDevice">一个按钮</span>
+        </Button>
+      </Col>
+    </Row>
+
+    <Menu mode="horizontal" :theme="theme" active-name="1" id="mainNav" @on-select="activeMenuItem($event)">
+      <Menu-item name="1" :class="['mainNav-item', {'mainNav-item-actived': activedMenuItem['1']}]">
+        <Icon type="ios-paper"></Icon>
+        首页
+      </Menu-item>
+      <Menu-item name="2" :class="['mainNav-item', {'mainNav-item-actived': activedMenuItem['2']}]">
+        <Icon type="ios-people"></Icon>
+        分类浏览
+      </Menu-item>
+    </Menu>
+    <router-view></router-view>
+    <div class="layout-copy">
+      2017-2018 &copy; Patrick
+    </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'app',
+      theme: 'dark',  //Menu 主题
+      search: '',     //搜索框值
+      notXsDevice: window.innerWidth > 768, //判断当前页面宽度
+      activedMenuItem: {"1": true, "2": false}, //Menu item 样式设置需要
+    }
+  },
+  methods: {
+    showNothing: function () {
+      this.$Message.info('啥都没有');
+    },
+    activeMenuItem: function (itemIndex) {
+      for (var item in this.activedMenuItem) {
+        if (itemIndex === item) {
+          this.activedMenuItem[item] = true;
+        } else {
+          this.activedMenuItem[item] = false;
+        }
+      }
+    }
+  },
+  mounted: function () {
+    var that = this;
+    window.onresize = function () {
+      that.notXsDevice = window.innerWidth > 768;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+}
+
+.Col-logo {
+  display: flex; 
+  align-items: center;
+}
+
+#mainNav {
+  height: 40px;
+  background-color: rgb(62, 61, 67);
+}
+
+.mainNav-item {
+  left: 53px;
+  line-height: 38px;
+  border-top: 2px solid rgba(0, 0, 0, 0);
+}
+
+.mainNav-item-actived, .mainNav-item:hover{
+  background-color: rgb(49, 48, 53);
+  border-top: 2px solid #27ae60;
+}
+</style>
+
+<style>
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,152 @@
+<template>
+  <Row type="flex" justify="center" align="top">
+    <Col :sm="{span: 4, offset: 1}">
+    <a href="" class="navItem navItem-odd">文学</a>
+    <a href="" class="navItem navItem-odd">流行</a>
+    <div></div>
+    <a href="" class="navItem navItem-even">文化</a>
+    <a href="" class="navItem navItem-even">生活</a>
+    <div></div>
+    <a href="" class="navItem navItem-odd">经管</a>
+    <a href="" class="navItem navItem-odd">科技</a>
+    <div></div>
+    <a href="" class="navItem navItem-even">哲学</a>
+    <a href="" class="navItem navItem-even">法学</a>
+    <div></div>
+    <a href="" class="navItem navItem-odd">教育学</a>
+    <a href="" class="navItem navItem-odd">历史学</a>
+    <div></div>
+    <a href="" class="navItem navItem-even">理学</a>
+    <a href="" class="navItem navItem-even">工学</a>
+    <div></div>
+    <a href="" class="navItem navItem-odd">农学</a>
+    <a href="" class="navItem navItem-odd">医学</a>
+    </Col>
+    <Col :sm="19" >
+      <Col :sm="18" id="row-1-col-2">
+      <Carousel v-model="carouselIndex" :autoplay="CarouselSetting.autoplay" :autoplay-speed="CarouselSetting.autoplaySpeed" :dots="CarouselSetting.dots"
+        :trigger="CarouselSetting.trigger" :arrow="CarouselSetting.arrow" class="main-carousel">
+        <Carousel-item>
+          <img src="../assets/img1.jpeg" alt="img1" class="carousel-img">
+        </Carousel-item>
+        <Carousel-item>
+          <img src="../assets/img2.jpeg" alt="img2" class="carousel-img">
+        </Carousel-item>
+        <Carousel-item>
+          <img src="../assets/img3.jpeg" alt="img3" class="carousel-img">
+        </Carousel-item>
+        <Carousel-item>
+          <img src="../assets/img4.jpeg" alt="img4" class="carousel-img">
+        </Carousel-item>
+      </Carousel>
+      </Col>
+      <Col :sm="{span: 5}">
+      <Card id="notice-card">
+        <p slot="title">一段文字</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio rerum laboriosam quia modi saepe vitae iusto,
+          nostrum ut, molestias incidunt vero, corrupti! </p>
+      </Card>
+      <Col :sm="1"></Col>
+    </Col>
+
+    <Card class="adcards">
+        <div style="text-align:center">
+            <img src="../assets/logo.png">
+            <h3>广告位</h3>
+        </div>
+    </Card>
+   <Card class="adcards" style="margin-left: 0">
+        <div style="text-align:center">
+            <img src="../assets/logo.png">
+            <h3>又一个广告位</h3>
+        </div>
+    </Card>
+    <Card class="adcards" style="margin-left: 0">
+        <div style="text-align:center">
+            <img src="../assets/logo.png">
+            <h3>还是广告位</h3>
+        </div>
+    </Card>
+    </Col>
+    
+  </Row>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'index',
+        theme: 'light',
+        carouselIndex: 0,
+        CarouselSetting: {
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: 'inside',
+          trigger: 'click',
+          arrow: 'hover',
+        },
+      }
+    },
+    methods: {
+      show: function (name) {
+        console.log(name);
+      },
+    }
+  }
+
+</script>
+
+<style scoped>
+  .navItem {
+    display: table-cell;
+    vertical-align: middle;
+    width: 108px;
+    height: 60px;
+    margin: auto;
+    padding: 0px;
+    font-size: 16px;
+    color: black;
+  }
+
+  .navItem:hover {
+    color: red;
+  }
+
+  .navItem-odd {
+    background-color: rgb(247, 246, 242);
+  }
+
+  .navItem-even {
+    background-color: rgb(252, 252, 250);
+  }
+
+  #row-1-col-2>* {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+
+  .carousel-img {
+    width: auto;
+    height: 260px;
+  }
+
+  #notice-card {
+    margin-top: 10px;
+    height: 250px;
+  }
+
+  .adcards {
+    display: inline-block;
+    float: left;
+    height: 150px;
+    width: 304px;
+    margin: 5px 20px 0px 20px;
+    text-align: center;
+    background-color: #ecf0f1;
+  }
+
+  .adcards img{
+    height: 80px;
+  }
+</style>
