f46adfdb6e604d7dcc237822801207395ce123bd
PatZhuang <pat_zhuang@outlook.com>
2017-04-28 09:16:42 +0800

    修正 merge 冲突

--- src/main.js
+++ src/main.js
@@ -3,15 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
+
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
-import {Rate, Carousel, CarouselItem} from 'element-ui'
-=======
-import iView from '../node_modules/iview'
-import '../node_modules/iview/dist/styles/iview.css'
 import ElementUI from 'element-ui'
->>>>>>> elementui
 
 Vue.config.productionTip = false
 
